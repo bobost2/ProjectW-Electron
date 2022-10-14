e889666616917cb8f6a4bd7bfdd3db309204bb12
Boris Stefanov <bobisfe@gmail.com>
2022-10-14 10:11:56 +0300

    Fixed React infinite loop problem. Deleted eslint.

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,52 +1,82 @@
+import React, { FC, useEffect, useState } from 'react';
 import AddUserComponent from 'components/User/AddUserComponent/AddUserComponent';
 import ExUserComponent from 'components/User/ExUserComponent/ExUserComponent';
 import WindowComponent from 'components/WindowComponent/WindowComponent';
-import React, { FC, useEffect, useState } from 'react';
 import styles from './MainPage.module.scss';
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
 
 interface MainPageProps {}
 
 const MainPage: FC<MainPageProps> = () => {
-  
   const [tempUsers, setTempUsers] = useState([{}]);
   const [animFadeAway, setAnimationFadeAway] = useState(false);
+  const [windowTitle, setWindowTitle] = useState("Select user:");
+  const [inCreateUser, setInCreateUser] = useState(false);  
 
-  function AddUser(){
+  function AddUser() {
     setAnimationFadeAway(true);
     setTimeout(() => {
+      setInCreateUser(true);
       setAnimationFadeAway(false);
-    }, 500)
+    }, 500);
   }
 
   useEffect(() => {
-    var tempArray = [];
-    tempArray.push({"avatar": null, "username": "user1"});
-    tempArray.push({"avatar": "#AVATAR#", "username": "user2"});
-    tempArray.push({"avatar": null, "username": "user3"});
-    tempArray.push({"avatar": null, "username": "user4"});
-    tempArray.push({"avatar": "#AVATAR#", "username": "user5"});
-    tempArray.push({"avatar": null, "username": "user6"});
-
+    const tempArray = [];
+    tempArray.push({ avatar: null, username: 'user1' });
+    tempArray.push({ avatar: '#AVATAR#', username: 'user2' });
+    tempArray.push({ avatar: null, username: 'user3' });
+    tempArray.push({ avatar: null, username: 'user4' });
+    tempArray.push({ avatar: '#AVATAR#', username: 'user5' });
+    tempArray.push({ avatar: null, username: 'user6' });
 
     setTempUsers(tempArray);
-  },[])
+  }, []);
 
-  return(
-    <div className={styles.MainPage}>
-      <WindowComponent title={"Select user:"} height={"33rem"} padding={"2rem"}>
-        <div style={{maxHeight: '26.2rem'}} className={animFadeAway ? styles.InsideWindowFade : styles.InsideWindow }>
-          <span onClick={AddUser}>
-            <AddUserComponent/>
-          </span>
-          
-          { tempUsers.map((user:any) => (
-            <span key={user.username + "_key"}>
-              <ExUserComponent avatar={user.avatar} username={user.username}/>
-            </span>
-          ))}
-        </div>
+  useEffect(() => {
+    if(inCreateUser){
+      setWindowTitle("Create user:");
+    }
+    else {
+      setWindowTitle("Select user:");
+    }
+  }, [inCreateUser]);
+
+  return (
+    <div>
+      <WindowComponent title={windowTitle} height="33rem" padding="2rem">
+          {
+            inCreateUser ? 
+              <>
+                <div
+                  className={
+                     animFadeAway ? styles.InsideWindowFade : styles.InsideWindow
+                  }>
+                      Create user component.
+                </div>
+              </>
+            : 
+              <>
+                <div
+                  style={{ maxHeight: '26.2rem' }}
+                  className={
+                    animFadeAway ? styles.InsideWindowFade : styles.InsideWindow
+                  }
+                >
+                  <span onClick={AddUser}>
+                    <AddUserComponent />
+                  </span>
+                
+                  {tempUsers.map((user: any) => (
+                    <span key={`${user.username}_key`}>
+                      <ExUserComponent avatar={user.avatar} username={user.username} />
+                    </span>
+                  ))}
+                </div>
+              </>
+          }
       </WindowComponent>
     </div>
   );
-}
+};
 export default MainPage;
