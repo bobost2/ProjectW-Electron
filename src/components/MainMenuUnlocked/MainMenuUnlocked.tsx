5710b4596df7aae6479f2b4fde98b2521f85d375
Boris Stefanov <bobisfe@gmail.com>
2022-10-21 17:35:40 +0300

    If userId does not exist, redirect to user selection screen.

--- src/components/MainMenuUnlocked/MainMenuUnlocked.tsx
+++ src/components/MainMenuUnlocked/MainMenuUnlocked.tsx
@@ -1,12 +1,18 @@
-import React, { FC } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { FC, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import styles from './MainMenuUnlocked.module.scss';
 
 interface MainMenuUnlockedProps {}
 
 const MainMenuUnlocked: FC<MainMenuUnlockedProps> = () => {
   
+  const navigate = useNavigate();
+  
   let userId = localStorage.getItem('userId');
+
+  useEffect(() => {
+    if(localStorage.getItem('userId') === null) navigate('/');
+  }, [])
   
   return (
     <div className={styles.MainMenuUnlocked}>
